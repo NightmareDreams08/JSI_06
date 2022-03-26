2b3c14ad64fd427f59cec1cec590c045b991aa28
NightmareDreams08 <101409168+NightmareDreams08@users.noreply.github.com>
2022-03-26 14:41:57 +0700

    Update app.js

--- lesson1/app.js
+++ lesson1/app.js
@@ -6,6 +6,7 @@
 // let {firstName,lastName,Age} = person
 // console.log(firstName,lastName,Age)
 import { sub, sum, divi, mul } from "./math.js";
+
 console.log ("sum",sum(10,10))
 console.log ("sub",sub(10,10))
 console.log ("mul",mul(10,10))
