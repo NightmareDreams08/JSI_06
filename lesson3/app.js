9fa23be9e733050a8f1af7e1cb24a65f0c5e0fef
NightmareDreams08 <101409168+NightmareDreams08@users.noreply.github.com>
2022-04-03 10:29:21 +0700

    lesson3

---
+++ lesson3/app.js
@@ -0,0 +1,29 @@
+let rootElement = document.getElementById(`root`);
+// // const domElement = document.createElement(`h1`)
+// // console.dir(domElement)
+// // const domul = document.createElement(`ul`)
+// // console.dir(domElement)
+// // const ulA = document.createElement(`li`)
+// // console.dir(domElement)
+// // const ulB = document.createElement(`li`)
+// // console.dir(domElement)
+// // const ulC = document.createElement(`li`)
+// // console.dir(domElement)
+// // domElement.innerText = `hello world`
+// // rootElement.appendChild(domElement)
+// function render
+
+console.log("react", React);
+let reactElement = React.createElement(
+  "h1",
+  {
+    classname: "a",
+    id: "a",
+    style: {
+      color: "red",
+    },
+  },
+  React.createElement("h1", {}, "://")
+);
+const rootReactElement = ReactDOM.createRoot(rootElement);
+rootReactElement.render(reactElement);
