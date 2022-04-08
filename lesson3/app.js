05fc680345610acf6de2a6601ac303a6b164eef8
NightmareDreams08 <101409168+NightmareDreams08@users.noreply.github.com>
2022-04-08 23:18:02 +0700

    Update app.js

--- lesson3/app.js
+++ lesson3/app.js
@@ -1,29 +1,31 @@
-let rootElement = document.getElementById(`root`);
-// // const domElement = document.createElement(`h1`)
-// // console.dir(domElement)
-// // const domul = document.createElement(`ul`)
-// // console.dir(domElement)
-// // const ulA = document.createElement(`li`)
-// // console.dir(domElement)
-// // const ulB = document.createElement(`li`)
-// // console.dir(domElement)
-// // const ulC = document.createElement(`li`)
-// // console.dir(domElement)
-// // domElement.innerText = `hello world`
-// // rootElement.appendChild(domElement)
-// function render
+// let rootElement = document.getElementById(`root`);
+// // // const domElement = document.createElement(`h1`)
+// // // console.dir(domElement)
+// // // const domul = document.createElement(`ul`)
+// // // console.dir(domElement)
+// // // const ulA = document.createElement(`li`)
+// // // console.dir(domElement)
+// // // const ulB = document.createElement(`li`)
+// // // console.dir(domElement)
+// // // const ulC = document.createElement(`li`)
+// // // console.dir(domElement)
+// // // domElement.innerText = `hello world`
+// // // rootElement.appendChild(domElement)
+// // function render
 
-console.log("react", React);
-let reactElement = React.createElement(
-  "h1",
-  {
-    classname: "a",
-    id: "a",
-    style: {
-      color: "red",
-    },
-  },
-  React.createElement("h1", {}, "://")
-);
-const rootReactElement = ReactDOM.createRoot(rootElement);
-rootReactElement.render(reactElement);
+// console.log("react", React);
+// let reactElement = React.createElement(
+//   "h1",
+//   {
+//     classname: "a",
+//     id: "a",
+//     style: {
+//       color: "red",
+//     },
+//   },
+//   React.createElement("h1", {}, "://")
+// );
+// const rootReactElement = ReactDOM.createRoot(rootElement);
+// rootReactElement.render(reactElement);
+const numbers = [1, 2, 3, 4, 5];
+let res = numbers.map((number) => number * 2);
