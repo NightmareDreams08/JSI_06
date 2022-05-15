9f59b752217a487e78a17252859c001607eb59d5
NightmareDreams08 <nhan100434@gmail.com>
2022-05-15 12:41:45 +0700

    ls-07

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+// import ToDoComponent from './components/ToDoComponent';
+// import Login from './components/DN';
+import DemoNextUI from './components/DemoNextUi';
+
+
+export default function App() {
+// const {isLogin, setIslogin} = useState(false);
+// console.log(useState)
+
+// const onCLickHandler = () =>{
+//   setIslogin(true)
+
+
+  return (
+  //    <div className='App'>
+  //      {isLogin === true ? <ToDoComponent/> : <Login/>}
+  //       <button onClick={onCLickHandler}>
+  //         {isLogin === true ? "Log out button":"Login button"}
+  //       </button>
+  //    </div>
+ <DemoNextUI/>
+  );
+}
+
---
+++ src/components/ToDoComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const ToDoComponent = () =>{
+   
+    return (
+       <div>
+        </div>
+    )
+}
+export default ToDoComponent;
